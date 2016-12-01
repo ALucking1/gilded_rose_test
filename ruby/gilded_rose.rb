31c5d51c3d489f5085d48312b526933fccd377b0
Anna Lucking <annavlucking@hotmail.co.uk>
2016-12-01 20:26:39 +0000

    some refactoring of tests

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -12,26 +12,24 @@ class GildedRose
 
       case item.name
       when "Aged Brie"
-        item.quality += 1 if item.quality < 50
+        item.quality += 1 if item.quality < 50 #1
       when "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality < 50
+        if item.quality < 50 #1
           if item.sell_in.between?(6, 10)
             item.quality += 2
           elsif item.sell_in.between?(1, 5)
             item.quality += 3
-          elsif item.sell_in = 0
+          else item.sell_in = 0
             item.quality = 0
           end
         end
       when "Sulfuras, Hand of Ragnaros"
-        item.quality = item.quality if item.quality < 50
+        item.quality = item.quality if item.quality < 50 #1
       else
         if item.quality.between?(1, 50)
           item.sell_in > 0 ? item.quality -= 1 : item.quality -= 2
         end
       end
-
     end
   end
-
 end
--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -3,35 +3,35 @@ require './item'
 
 describe GildedRose do
 
-  describe "#update_quality" do
+  let(:subject) {GildedRose.new(item)}
+  let(:item) {[Item.new("foo", 10, 10)]}
+
+  describe "example kata test" do
     it "does not change the name" do
-      item = [Item.new("foo", 0, 0)]
-      GildedRose.new(item).update_quality
+      subject.update_quality
       expect(item[0].name).to eq "foo"
     end
   end
 
   context "sell in value" do
     it "drops by 1 each time update_quality is run" do
-      item = [Item.new("test", 10, 10)]
-      GildedRose.new(item).update_quality
+      subject.update_quality
       expect(item[0].sell_in).to eq(9)
     end
   end
 
-  context "quality value" do
-    it "drops by 1 each time update_value is run" do
-      item = [Item.new("test", 10, 10)]
+  context "sell in date has passed" do
+    it 'quality degrades twice as fast' do
+      item = [Item.new("test", 0, 6)]
       GildedRose.new(item).update_quality
-      expect(item[0].quality).to eq(9)
+      expect(item[0].quality).to eq(4)
     end
   end
 
-  context "sell by date has passed" do
-    it 'quality degrades twice as fast' do
-      item = [Item.new("test", -1, 6)]
-      GildedRose.new(item).update_quality
-      expect(item[0].quality).to eq(4)
+  context "quality value" do
+    it "drops by 1 each time update_quality is run" do
+      subject.update_quality
+      expect(item[0].quality).to eq(9)
     end
   end
 
@@ -44,6 +44,14 @@ describe GildedRose do
   end
 
   context "quality of an item" do
+    it "is never more than 50" do
+      item = [Item.new("Aged Brie", 10, 50)]
+      GildedRose.new(item).update_quality
+      expect(item[0].quality).to eq(50)
+    end
+  end
+
+  context "sell_in of an item" do
     it "is never negative" do
       item = [Item.new("test", 0, 5)]
       GildedRose.new(item).update_quality
@@ -56,14 +64,6 @@ describe GildedRose do
       item = [Item.new("Aged Brie", 5, 39)]
       GildedRose.new(item).update_quality
       expect(item[0].quality).to eq(40)
-    end
-  end
-
-  context "quality of an item" do
-    it "is never more than 50" do
-      item = [Item.new("Aged Brie", 10, 50)]
-      GildedRose.new(item).update_quality
-      expect(item[0].quality).to eq(50)
     end
   end
 
